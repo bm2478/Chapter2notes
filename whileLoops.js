ce2896b06b1332bedddbd048e4f59d9f2b95ae0e
7xge <7xge@fresnou.org>
2023-02-17 14:14:21 -0800

    initial commit

---
+++ whileLoops.js
@@ -0,0 +1,29 @@
+const readLine = require('readline-sync');
+
+// let x = 0;
+// while (x < 11){
+//     console.log(x);
+//     x++;
+// }
+
+// let y = 0;
+// while (y < 11){
+//     y++;
+//     console.log(y);
+// }
+
+// let z = 10;
+// while (z > 5){
+//     console.log(z);
+//     z--;
+// }
+
+let start=0;
+while (start <= 100){
+    if(start < 10){
+        console.log(start);
+    } else if (start > 25){
+        console.log(start);
+    }
+    start++;
+}
---
+++ whileLoops1.js
@@ -0,0 +1,38 @@
+const readLine = require('readline-sync');
+
+//1
+let x = 0;
+while (x < 26){
+    console.log(x);
+    x++;
+}
+
+//2
+let a = 0;
+while (a < 51){
+    console.log(a);
+    a++;
+}
+
+//3
+let b = -10;
+while (b < 11){
+    console.log(b);
+    b++;
+}
+
+//4
+let c = 0;
+while (c > -51){
+    console.log(c);
+    c--;
+}
+
+//5
+let d = 0;
+while (d < 101){
+    console.log(d);
+    d+=2;
+}
+
+//6
