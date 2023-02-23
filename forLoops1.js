03f270087c0154f2376a6776d8a5f179d38a8cd9
7xge <7xge@fresnou.org>
2023-02-23 14:19:48 -0800

    finished 1-5

---
+++ forLoops1.js
@@ -0,0 +1,28 @@
+//1
+// for (let i=0; i < 151; i++){
+//     console.log(i);
+// }
+
+//2
+// for (let i=0; i < 41; i++){
+//     console.log(i**2);
+// }
+
+//3
+// for (let i=0; i < 30; i++){
+//     console.log(i**3);
+// }
+
+//4
+// for(let i = 0; i<= 700; i++){
+//     if(i%5==0){
+//     console.log(i);
+//     }
+// }
+
+//5
+for(let i = 350; i<= 1000; i++){
+    if(i%10==0){
+        console.log(i);
+    }
+}
