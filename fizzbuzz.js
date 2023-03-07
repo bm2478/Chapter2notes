74ee6a6efd44021c0627286ce154e3f74fb62ddf
7xge <7xge@fresnou.org>
2023-03-07 14:18:10 -0800

    finished fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+for (let x=0; x < 100; x++) {
+    if (x % 15 == 0){
+        console.log("fizzbuzz")
+    }else if (x % 5 == 0){
+        console.log("buzz")
+    }else if (x % 3 == 0){
+        console.log("fizz")
+    }
+    else{
+        console.log(x);
+    }
+}
